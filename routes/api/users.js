13ff93e8e25dc6750ef9007c8694f5475dc93243
matthewdjoseph <matthewjoseph.student@careerdevs.com>
2019-12-28 12:33:40 -0500

    added login authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 // @route   POST api/users
 // @desc    Register user
