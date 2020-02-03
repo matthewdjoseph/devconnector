4a9292569606c3bb9964ab261c66d1f8771a3cc9
matthewdjoseph <matthewjoseph.student@careerdevs.com>
2020-02-03 16:41:01 -0500

    WIP: user authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -9,8 +9,19 @@ import './App.css';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './actions/auth';
 
-const App = () => (
+if (localStorage.token){
+  setAuthToken(localStorage.token);
+};
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return(
   <Provider store={store}>
     <Router>
       <Fragment>
@@ -26,6 +37,6 @@ const App = () => (
       </Fragment>
     </Router>
   </Provider>
-);
+)};
 
 export default App;
