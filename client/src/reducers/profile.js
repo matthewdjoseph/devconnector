fde3b85c9cd302a313ed6f058be6e594b709ec62
matthewdjoseph <matthewjoseph.student@careerdevs.com>
2020-02-09 13:56:40 -0500

    added get profiles actions

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -22,6 +22,12 @@ export default function(state = initialState, action){
                 profile: payload,
                 loading: false
             };
+        case GET_PROFILES:
+            return {
+                ...state,
+                profiles: payload,
+                loading: false
+            }
         case PROFILE_ERROR:
             return {
                 ...state,
@@ -33,6 +39,12 @@ export default function(state = initialState, action){
                 ...state,
                 profile: null,
                 repos: [],
+                loading: false
+            };
+        case GET_REPOS:
+            return {
+                ...state,
+                repos: payload,
                 loading: false
             };
         default:
